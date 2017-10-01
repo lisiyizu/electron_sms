5c870c4d937ba07f7e9a8a60bb3fb0d944526d0a
Brian Zimmerman <briangzimmerman@gmail.com>
2017-10-01 01:04:00 -0500

    added initial retrieval of sms threads

---
+++ db/models/thread.js
@@ -0,0 +1,113 @@
+const mongoose = require('mongoose');
+const _ = require('lodash');
+
+let threadSchema = new mongoose.Schema({
+    pb_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    last_updated: {
+        type: Number,
+        required: true
+    },
+    recipients: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            address: {
+                type: String,
+                required: true
+            },
+            number: {
+                type: String,
+                required: true
+            },
+            image_url: {
+                type: String,
+                required: false
+            }
+        }
+    ],
+    messages: [{
+        timestamp: {
+            type: Number,
+            required: true
+        },
+        direction: {
+            type: String,
+            required: true
+        },
+        recipient_index: {
+            type: Number,
+            required: true
+        },
+        body: {
+            type: String
+        },
+        image_urls: []
+    }]
+});
+
+let sortMessages = (a, b) => {
+    return a.timestamp - b.timestamp;
+};
+
+threadSchema.methods.updateMessages = function(messages){
+    let thread = this;
+    let last_updated = thread.last_updated;
+    let toAdd = [];
+    messages.forEach((message) => {
+        if(message.timestamp > last_updated)
+            last_updated = message.timestamp;
+        let newMessage = {
+            timestamp: message.timestamp,
+            recipient_index: message.recipient_index ? message.recipient_index : 0,
+            body: message.body,
+            image_urls: message.image_urls ? message.image_urls : [],
+            direction: message.direction
+        };
+        if(_.findIndex(thread.messages, newMessage) == -1)
+            toAdd.push(newMessage);
+    });
+    thread.last_updated = last_updated;
+    thread.messages = thread.messages.concat(toAdd);
+    return thread.save();
+};
+
+threadSchema.statics.findOutdated = function(threads){
+    let Thread = this;
+    let promises = [];
+    let finds = []
+    threads.forEach((thread) => {
+        finds.push(Thread.find({
+            pb_id: thread.id
+        })); 
+    });
+    return Promise.all(finds)
+        .then((foundThreads) => {
+            for(let idx = 0; idx < foundThreads.length; idx++) {
+                let foundThread = foundThreads[idx];
+                if (foundThread.length > 0) {
+                    if(foundThread[0].last_updated < threads[idx].latest.timestamp)
+                        promises.push(foundThread[0]);
+                } else {
+                    let foundThread = new Thread({
+                        pb_id: threads[idx].id,
+                        recipients: threads[idx].recipients,
+                        last_updated: threads[idx].latest.timestamp
+                    });
+                    promises.push(foundThread.save());
+                }
+            }
+            return Promise.all(promises);
+        });
+}
+
+let Thread = mongoose.model('Thread', threadSchema);
+
+module.exports = {
+    Thread
+};
--- main.js
+++ main.js
@@ -1,10 +1,86 @@
 const electron = require('electron')
 const WebSocket = require('ws');
+const request = require('request-promise');
+const url = require('url');
+const path = require('path');
+
 const config = require('./config/config.json');
+const { app, BrowserWindow, Main, ipcMain } = electron;
+const { mongoose } = require('./db/mongoose');
+const { Thread } = require('./db/models/thread');
 
-const {app, BrowswerWindow, Main, ipcMain} = electron;
-
-let socket = new WebSocket(`wss://stream.pushbullet.com/websocket/${config['access-token']}`);
+const api_url = 'https://api.pushbullet.com';
+/*
+let socket = new WebSocket(`wss://stream.pushbullet.com/websocket/${config['access_token']}`);
 socket.onmessage = (message) => {
-    console.log(message.data);
-};
+    let data = JSON.parse(message.data);
+    if(data.type === 'push' && data.push.type === 'sms_changed') {
+        let thread_id = data.push.notifications[0].thread_id;
+        request({
+            url: api_url + `/v2/permanents/${config.device_iden}_thread_${thread_id}`,
+            headers: {'Access-Token': config.access_token}
+        }, (err, res, body) => {
+            if(!err) {
+                Thread.find({pb_id: thread_id})
+                    .then((thread) => {
+                        if(thread) {
+                            return thread.updateMessages(JSON.parse(body));
+                        }
+                    })
+                    .then((thread_data) => {
+                        if(thread_data){
+                            console.log(thread_data);
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
+            }
+        });
+    }
+};*/
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    mainWindow.on('close', () => {
+        app.quit();
+    });
+
+    mainWindow.setMenu(null); //For No menu
+
+    //On startup update/find all texts
+    request({
+        uri: `${api_url}/v2/permanents/${config.device_iden}_threads`,
+        headers: { 'Access-Token': config.access_token },
+        json: true
+    })
+        .then((res) => {
+            return Thread.findOutdated(res.threads);
+        })
+        .then((threads) => {
+            let promises = [];
+            threads.forEach((thread) => {
+                promises.push(request({
+                    uri: api_url + `/v2/permanents/${config.device_iden}_thread_${thread.pb_id}`,
+                    headers: { 'Access-Token': config.access_token },
+                    json: true
+                }).then((messages) => {
+                    return thread.updateMessages(messages.thread);
+                }));
+            });
+            return Promise.all(promises)
+        })
+        .then(() => {
+            return Thread.find({});
+        })
+        .then((threads) => {
+            console.log(threads);
+        })
+});
